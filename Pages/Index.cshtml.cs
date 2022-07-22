8f4ba0d8a852cfcb5ec5f33d3b6c70224c20372e
Landon Pierce <landonpierce@microsoft.com>
2022-07-22 14:04:35 -0400

    add key vault demo

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace app_service_demo.Pages;
@@ -6,14 +8,27 @@ namespace app_service_demo.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    private readonly IConfiguration _config;
+    private readonly SecretClient _keyVaultSecretClient;
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
     {
         _logger = logger;
+        _config = config;
+        // You should use dependency injection here to inject this client. Creating it on every request as shown here is expensive, but is fine for demo purposes.
+        _keyVaultSecretClient = new SecretClient(new Uri(_config["KeyVault:Url"]), 
+       // https://docs.microsoft.com/en-us/dotnet/api/azure.identity.defaultazurecredential?view=azure-dotnet 
+        new DefaultAzureCredential());
     }
 
-    public void OnGet()
+    public async Task OnGet()
     {
-
+        // Option 1:
+        // Pulls a secret from config (Injected from Key Vault on line 9 of Program.cs)
+        ViewData["Secret1"] = _config["Secret1"];
+        
+        // Option 2:
+        // Pulls a secret directly from KeyVault using the KeyVault SDK: https://www.nuget.org/packages/Azure.Security.KeyVault.Secrets/
+        var secret = await _keyVaultSecretClient.GetSecretAsync("secret2");
+        ViewData["Secret2"] = secret.Value.Value; 
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,20 @@
+using Azure.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
+
+try
+{
+    builder.Configuration.AddAzureKeyVault(new Uri(builder.Configuration["KeyVault:Url"]), 
+    // https://docs.microsoft.com/en-us/dotnet/api/azure.identity.defaultazurecredential?view=azure-dotnet 
+    new DefaultAzureCredential());
+
+}
+catch (Exception e)
+{
+    // Do not do this in a real scenario. We are "swallowing" the error here for the purpose of demonstration. 
+    // In a real scenario, you probably would want your app to fail startup if it cannot connect to keyvault for whatever reason (bad URI, no auth, etc)
+    Console.WriteLine("Exception encountered adding azure key vault");
+}
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -11,6 +27,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
 }
 
 app.UseHttpsRedirection();
